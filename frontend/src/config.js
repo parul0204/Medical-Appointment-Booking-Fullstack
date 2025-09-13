17f8040c364a340bffc1e5c8689a99968aa480d8
parul0204 <2k23.cs2312483@gmail.com>
2025-09-13 13:50:05 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 const url_production = "https://medical-appointment-booking-fullstack.onrender.com"
 // eslint-disable-next-line no-unused-vars
 const url_local = "http://localhost:5000"
-export const BASE_URL = url_local;
+export const BASE_URL = url_production;
 export const token = localStorage.getItem('token');
 
 
