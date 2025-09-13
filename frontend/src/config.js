9be609a15f24c810d721d45a5b51d6813362cd59
parul0204 <2k23.cs2312483@gmail.com>
2025-09-13 13:55:30 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-const url_production = "https://medical-appointment-booking-fullstack.onrender.com"
+const url_production = "https://medical-appointment-backend-3viy.onrender.com"
 // eslint-disable-next-line no-unused-vars
 const url_local = "http://localhost:5000"
 export const BASE_URL = url_production;
